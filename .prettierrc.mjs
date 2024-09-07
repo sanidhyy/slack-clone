90f39fdd61870505d50ae838327c476110d3c5b9
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-09-07 14:08:26 +0000

    prettier and eslint setup

---
+++ .prettierrc.mjs
@@ -0,0 +1,31 @@
+import { parsers as tailwindPluginParsers } from 'sort-classes';
+import pluginSortImports from 'tidy-imports';
+
+/**
+ * @refs  https://github.com/tailwindlabs/prettier-plugin-tailwindcss/issues/31#issuecomment-1195411734
+ */
+/** @type {import("prettier").Parser}  */
+const bothParser = {
+  ...tailwindPluginParsers.typescript,
+  preprocess: pluginSortImports.parsers.typescript.preprocess,
+};
+
+/** @type {import("prettier").Plugin}  */
+const mixedPlugin = {
+  parsers: {
+    typescript: bothParser,
+  },
+  options: {
+    ...pluginSortImports.options,
+  },
+};
+
+export default {
+  plugins: [mixedPlugin],
+  semi: true,
+  singleQuote: true,
+  printWidth: 140,
+  importOrder: ['<THIRD_PARTY_MODULES>', '^@/(.*)$', '^[./]'],
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
+};
