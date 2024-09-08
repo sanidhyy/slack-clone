25fc1499666e8ede757586c62d486b4c4b51b661
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-09-08 09:06:54 +0000

    sidebar links added

---
+++ src/app/workspace/[workspaceId]/sidebar-button.tsx
@@ -0,0 +1,23 @@
+import type { LucideIcon } from 'lucide-react';
+import type { IconType } from 'react-icons/lib';
+
+import { Button } from '@/components/ui/button';
+import { cn } from '@/lib/utils';
+
+interface SidebarButtonProps {
+  icon: LucideIcon | IconType;
+  label: string;
+  isActive?: boolean;
+}
+
+export const SidebarButton = ({ icon: Icon, label, isActive = false }: SidebarButtonProps) => {
+  return (
+    <div className="flex flex-col items-center justify-center gap-y-0.5 cursor-pointer group">
+      <Button variant="transparent" className={cn('size-9 p-2 group-hover:bg-accent/20', isActive && 'bg-accent/20')}>
+        <Icon className="size-5 text-white group-hover:scale-110 transition-all" />
+      </Button>
+
+      <span className="text-[11px] text-white group-hover:text-accent">{label}</span>
+    </div>
+  );
+};
--- src/app/workspace/[workspaceId]/sidebar.tsx
+++ src/app/workspace/[workspaceId]/sidebar.tsx
@@ -1,11 +1,22 @@
+import { Bell, Home, MessagesSquare, MoreHorizontal } from 'lucide-react';
+import { usePathname } from 'next/navigation';
+
 import { UserButton } from '@/features/auth/components/user-button';
 
+import { SidebarButton } from './sidebar-button';
 import { WorkspaceSwitcher } from './workspace-switcher';
 
 export const Sidebar = () => {
+  const pathname = usePathname();
+
   return (
     <aside className="w-[70px] h-full bg-[#381349] flex flex-col gap-y-4 items-center pt-[9px] pb-[4px]">
       <WorkspaceSwitcher />
+
+      <SidebarButton icon={Home} label="Home" isActive={pathname.includes('/workspace')} />
+      <SidebarButton icon={MessagesSquare} label="DMs" />
+      <SidebarButton icon={Bell} label="Activity" />
+      <SidebarButton icon={MoreHorizontal} label="More" />
 
       <div className="flex flex-col items-center justify-center gap-y-1 mt-auto">
         <UserButton />
