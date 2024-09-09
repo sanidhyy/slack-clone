6e63d5f383c6bbb84819e28d13e89dbabb3e4a7e
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-09-09 15:38:18 +0000

    members functionality implemented

--- convex/schema.ts
+++ convex/schema.ts
@@ -9,6 +9,14 @@ const schema = defineSchema({
     userId: v.id('users'),
     joinCode: v.string(),
   }),
+  members: defineTable({
+    userId: v.id('users'),
+    workspaceId: v.id('workspaces'),
+    role: v.union(v.literal('admin'), v.literal('member')),
+  })
+    .index('by_user_id', ['userId'])
+    .index('by_workspace_id', ['workspaceId'])
+    .index('by_workspace_id_user_id', ['workspaceId', 'userId']),
 });
 
 export default schema;
--- convex/workspaces.ts
+++ convex/workspaces.ts
@@ -3,6 +3,12 @@ import { v } from 'convex/values';
 
 import { mutation, query } from './_generated/server';
 
+const generateCode = () => {
+  const code = Array.from({ length: 6 }, () => '0123456789abcdefghijklmnopqrstuvwxyz'[Math.floor(Math.random() * 36)]).join('');
+
+  return code;
+};
+
 export const create = mutation({
   args: {
     name: v.string(),
@@ -13,12 +19,18 @@ export const create = mutation({
     if (!userId) throw new Error('Unauthorized.');
 
     // TODO: create proper method later
-    const joinCode = '123456';
+    const joinCode = generateCode();
 
     const workspaceId = await ctx.db.insert('workspaces', {
       name: args.name,
       userId,
       joinCode,
+    });
+
+    await ctx.db.insert('members', {
+      userId,
+      workspaceId,
+      role: 'admin',
     });
 
     return workspaceId;
@@ -28,7 +40,30 @@ export const create = mutation({
 export const get = query({
   args: {},
   handler: async (ctx) => {
-    return await ctx.db.query('workspaces').collect();
+    const userId = await getAuthUserId(ctx);
+
+    if (!userId) {
+      return [];
+    }
+
+    const members = await ctx.db
+      .query('members')
+      .withIndex('by_user_id', (q) => q.eq('userId', userId))
+      .collect();
+
+    const workspaceIds = members.map((member) => member.workspaceId);
+
+    const workspaces = [];
+
+    for (const workspaceId of workspaceIds) {
+      const workspace = await ctx.db.get(workspaceId);
+
+      if (workspace) {
+        workspaces.push(workspace);
+      }
+    }
+
+    return workspaces;
   },
 });
 
@@ -38,6 +73,13 @@ export const getById = query({
     const userId = await getAuthUserId(ctx);
 
     if (!userId) throw new Error('Unauthorized.');
+
+    const member = await ctx.db
+      .query('members')
+      .withIndex('by_workspace_id_user_id', (q) => q.eq('workspaceId', args.id).eq('userId', userId))
+      .unique();
+
+    if (!member) return null;
 
     return await ctx.db.get(args.id);
   },
