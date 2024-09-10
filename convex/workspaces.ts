b3d5b22a05b179f87d52e9e54a8f58a550722f3f
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-09-10 11:51:49 +0000

    remove and update workspace api handler implemented

--- convex/workspaces.ts
+++ convex/workspaces.ts
@@ -82,3 +82,59 @@ export const getById = query({
     return await ctx.db.get(args.id);
   },
 });
+
+export const update = mutation({
+  args: {
+    id: v.id('workspaces'),
+    name: v.string(),
+  },
+  handler: async (ctx, args) => {
+    const userId = await getAuthUserId(ctx);
+
+    if (!userId) throw new Error('Unauthorized.');
+
+    const member = await ctx.db
+      .query('members')
+      .withIndex('by_workspace_id_user_id', (q) => q.eq('workspaceId', args.id).eq('userId', userId))
+      .unique();
+
+    if (!member || member.role !== 'admin') throw new Error('Unauthorized.');
+
+    await ctx.db.patch(args.id, {
+      name: args.name,
+    });
+
+    return args.id;
+  },
+});
+
+export const remove = mutation({
+  args: {
+    id: v.id('workspaces'),
+  },
+  handler: async (ctx, args) => {
+    const userId = await getAuthUserId(ctx);
+
+    if (!userId) throw new Error('Unauthorized.');
+
+    const member = await ctx.db
+      .query('members')
+      .withIndex('by_workspace_id_user_id', (q) => q.eq('workspaceId', args.id).eq('userId', userId))
+      .unique();
+
+    if (!member || member.role !== 'admin') throw new Error('Unauthorized.');
+
+    const [members] = await Promise.all([
+      ctx.db
+        .query('members')
+        .withIndex('by_workspace_id', (q) => q.eq('workspaceId', args.id))
+        .collect(),
+    ]);
+
+    for (const member of members) await ctx.db.delete(member._id);
+
+    await ctx.db.delete(args.id);
+
+    return args.id;
+  },
+});
--- src/features/workspaces/api/use-update-workspace.ts
+++ src/features/workspaces/api/use-update-workspace.ts
@@ -4,7 +4,7 @@ import { useCallback, useMemo, useState } from 'react';
 import { api } from '@/../convex/_generated/api';
 import { Id } from '@/../convex/_generated/dataModel';
 
-type RequestType = { name: string };
+type RequestType = { id: Id<'workspaces'>; name: string };
 type ResponseType = Id<'workspaces'> | null;
 
 type Options = {
@@ -14,7 +14,7 @@ type Options = {
   throwError?: boolean;
 };
 
-export const useCreateWorkspace = () => {
+export const useUpdateWorkspace = () => {
   const [data, setData] = useState<ResponseType>(null);
   const [error, setError] = useState<Error | null>(null);
   const [status, setStatus] = useState<'success' | 'error' | 'settled' | 'pending' | null>(null);
@@ -24,7 +24,7 @@ export const useCreateWorkspace = () => {
   const isError = useMemo(() => status === 'error', []);
   const isSettled = useMemo(() => status === 'settled', []);
 
-  const mutation = useMutation(api.workspaces.create);
+  const mutation = useMutation(api.workspaces.update);
 
   const mutate = useCallback(
     async (values: RequestType, options?: Options) => {
