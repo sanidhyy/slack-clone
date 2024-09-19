48f35ffa4b7ab082fc9fbc0e736647a2ca7fde98
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-09-19 07:18:44 +0000

    infinite load message added

--- src/components/message-list.tsx
+++ src/components/message-list.tsx
@@ -1,4 +1,5 @@
 import { differenceInMinutes, format, isToday, isYesterday } from 'date-fns';
+import { Loader } from 'lucide-react';
 import { useState } from 'react';
 
 import { useCurrentMember } from '@/features/members/api/use-current-member';
@@ -109,6 +110,34 @@ export const MessageList = ({
           })}
         </div>
       ))}
+
+      <div
+        className="h-1"
+        ref={(el) => {
+          if (el) {
+            const observer = new IntersectionObserver(
+              ([entry]) => {
+                if (entry.isIntersecting && canLoadMore) loadMore();
+              },
+              { threshold: 1.0 },
+            );
+
+            observer.observe(el);
+
+            return () => observer.disconnect();
+          }
+        }}
+      />
+
+      {isLoadingMore && (
+        <div className="text-center my-2 relative">
+          <hr className="absolute top-1/2 left-0 right-0 border-t border-gray-300" />
+
+          <span className="relative inline-block bg-white px-4 py-1 rounded-full text-xs border border-gray-300 shadow-sm">
+            <Loader className="size-4 animate-spin" />
+          </span>
+        </div>
+      )}
 
       {variant === 'channel' && channelName && channelCreationTime && <ChannelHero name={channelName} creationTime={channelCreationTime} />}
     </div>
