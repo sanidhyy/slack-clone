c60ba9dfe7d46f4f75d8f81eac3f534eb3d0d235
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-09-21 06:29:30 +0000

    site configuration added

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,3 +1,51 @@
+import type { Metadata } from 'next';
+
+export const siteConfig: Metadata = {
+  title: 'Slack Clone',
+  description:
+    'Collaborate with your team using real-time messaging, rich text editing, and emoji support in this Slack-like app built with Next.js, Convex, and Shadcn UI.',
+  keywords: [
+    'reactjs',
+    'nextjs',
+    'convex',
+    'next-auth',
+    'emoji-picker-react',
+    'lucide-icons',
+    'react-icons',
+    'quill-editor',
+    'shadcn-ui',
+    'radix-ui',
+    'tailwindcss',
+    'nuqs',
+    'sonner',
+
+    'typescript',
+    'javascript',
+    'vercel',
+    'postcss',
+    'prettier',
+    'eslint',
+    'react-dom',
+    'html',
+    'css',
+
+    'state-management',
+    'real-time-messaging',
+    'collaboration',
+    'ui/ux',
+    'slack-clone',
+
+    'date-fns',
+    'cn',
+    'clsx',
+    'lucide-react',
+  ] as Array<string>,
+  authors: {
+    name: 'Sanidhya Kumar Verma',
+    url: 'https://github.com/sanidhyy',
+  },
+} as const;
+
 export const links = {
   sourceCode: 'https://github.com/sanidhyy/slack-clone',
 } as const;
