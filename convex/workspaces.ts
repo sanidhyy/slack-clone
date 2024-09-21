2492e5b0d0010204c03d9c03e4060628d724f90c
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-09-21 08:10:49 +0000

    fixed workspace query

--- convex/workspaces.ts
+++ convex/workspaces.ts
@@ -159,7 +159,7 @@ export const getById = query({
   handler: async (ctx, args) => {
     const userId = await getAuthUserId(ctx);
 
-    if (!userId) throw new Error('Unauthorized.');
+    if (!userId) return null;
 
     const member = await ctx.db
       .query('members')
