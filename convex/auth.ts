89268f0919dcc50c9b045fadb5c795d75696aa93
Sanidhya Kumar Verma <sanidhyyy@gmail.com>
2026-08-19 22:27:34 +0530

    chore(deps): uipdate @convex-dev/auth and convex to latest version

--- convex/auth.ts
+++ convex/auth.ts
@@ -14,6 +14,6 @@ const CustomPassword = Password<DataModel>({
   },
 });
 
-export const { auth, signIn, signOut, store } = convexAuth({
+export const { auth, signIn, signOut, store, isAuthenticated } = convexAuth({
   providers: [CustomPassword, GitHub, Google],
 });
